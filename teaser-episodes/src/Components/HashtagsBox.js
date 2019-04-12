0a9d3a6984c7edea279c2f9fc75bb76e1f4907f6
omid-incarnation <mohammad.omidi1988@gmail.com>
2019-04-12 22:12:56 +0200

    Improve Hashtag tag

--- teaser-episodes/src/Components/HashtagsBox.js
+++ teaser-episodes/src/Components/HashtagsBox.js
@@ -2,21 +2,17 @@ import React, {Component} from 'react'
 
 export default class HashtagsBox extends Component {
     render() {
+        const hashtagsList = this
+            .props
+            .hashtags
+            .map(hashtag => <li
+                className="list-inline-item border border-primary rounded px-2 mr-2 my-1  text-primary">
+                {hashtag}
+            </li>)
         return (
             <React.Fragment>
                 <ul className="list-unstyled list-inline mt-5">
-                    <li
-                        className="list-inline-item border border-primary rounded px-2 mr-2 my-1  text-primary">
-                        {this.props.hashtags[0]}
-                    </li>
-                    <li
-                        className="list-inline-item border border-primary rounded px-2 mr-2 my-1 text-primary">
-                        {this.props.hashtags[1]}
-                    </li>
-                    <li
-                        className="list-inline-item border border-primary rounded px-2 mr-2 my-1 text-primary">
-                        {this.props.hashtags[2]}
-                    </li>
+                    {hashtagsList}
                 </ul>
             </React.Fragment>
         )
