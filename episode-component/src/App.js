a010cc9ba3d4298f8c195d2cf1fd9a3b0d322de9
omid-incarnation <mohammad.omidi1988@gmail.com>
2019-04-18 13:23:04 +0200

    Teaser Box is Added

--- episode-component/src/App.js
+++ episode-component/src/App.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 import EpisodeBox from './EpisodeComponent/EpisodeBox';
+import TeaserBox from './EpisodeComponent/TeaserBox';
 
 import './App.css';
+
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <EpisodeBox />
+                <TeaserBox />
             </div>
         );
     }
