c2906d020fd60cb79182672052113e23cdb17b00
omid-incarnation <mohammad.omidi1988@gmail.com>
2019-05-14 14:12:15 +0200

    index file is improved!

--- rest_api_test_02/index.js
+++ rest_api_test_02/index.js
@@ -5,15 +5,10 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
 
-const mysqlConnection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "test_schema",
-})
+const mysqlConnection = mysql.createConnection({host: "localhost", user: "root", password: "password", database: "test_schema", multipleStatements: true})
 
 mysqlConnection.connect((err) => {
-    if(!err){
+    if (!err) {
         console.log("DB connectin succeded :)")
     } else {
         console.log("DB connection failed")
@@ -25,7 +20,7 @@ app.listen(3000, () => console.log('Express sercer is running at port: 3000'))
 //get all users!
 app.get('/users', (req, res) => {
     mysqlConnection.query('select * from users', (err, rows, fields) => {
-        if(!err){
+        if (!err) {
             res.send(rows)
         } else {
             console.log(err)
@@ -35,8 +30,8 @@ app.get('/users', (req, res) => {
 
 //get all user by name!
 app.get('/users/:name', (req, res) => {
-    mysqlConnection.query('select * from users where userName = ?',[req.params.name], (err, rows, fields) => {
-        if(!err){
+    mysqlConnection.query('select * from users where userName = ?', [req.params.name], (err, rows, fields) => {
+        if (!err) {
             res.send(rows)
         } else {
             console.log(err)
@@ -46,9 +41,22 @@ app.get('/users/:name', (req, res) => {
 
 //delete a user by ID!
 app.delete('/users/:id', (req, res) => {
-    mysqlConnection.query('delete from users where ID = ?',[req.params.id],(err, rows) => {
-        if(!err){
+    mysqlConnection.query('delete from users where ID = ?', [req.params.id], (err, rows) => {
+        if (!err) {
             res.send('Deleted Successfully :)')
+        } else {
+            res.send('Something is wrong!')
+        }
+    })
+})
+
+//insert a user
+app.delete('/users', (req, res) => {
+    let userMaker = req.body
+    let sql = 'set ID = ?; set userName = ?; set userEmail = ?; set userPhone = ?; call userMaker(ID, userName, userEmail, userPhone);'
+    mysqlConnection.query(sql,[userMaker.ID, userMaker.userName, userMaker.userEmail, userMaker.userPhone], (err, rows) => {
+        if (!err) {
+            res.send(rows, console.log("Done"))
         } else {
             res.send('Something is wrong!')
         }
